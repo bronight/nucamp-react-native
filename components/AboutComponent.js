1ab6dd3ebc36866829a03160d3147c1759fd265a
Louis Williams <louawilliams@outlook.com>
2020-05-01 22:09:23 -0400

    Forms

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Card } from 'react-native-elements';
-import { View, Text, ScrollView, FlatList } from 'react-native';
+import { Text, ScrollView, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -14,15 +14,13 @@ const mapStateToProps = state => {
 
 function Mission () {
     return(
-        <ScrollView>
-            <Card
-                title={'Our Mission'}
-                wrapperStyle={{margin: 10}}>
-                <Text style={{marginBottom: 10}}>
-                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-                </Text>
-            </Card>
-        </ScrollView>
+        <Card
+            title={'Our Mission'}
+            wrapperStyle={{margin: 10}}>
+            <Text style={{marginBottom: 10}}>
+            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
     );
 }
 
